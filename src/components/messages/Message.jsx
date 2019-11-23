ac192dda806f7fe9e8d922a5767ba07b87d9041a
LilBudj <kinich2001@gmail.com>
2019-11-23 17:38:36 +0300

    Toggle

--- src/components/messages/Message.jsx
+++ src/components/messages/Message.jsx
@@ -7,17 +7,15 @@ class Message extends Component {
     state = {
         isActivated: false
     };
-    activate = () => {
-        this.setState({isActivated: true})
-    };
-    deactivate = () => {
-        this.setState({isActivated: false})
+    toggle = () => {
+        let toggle = !this.state.isActivated;
+        this.setState({isActivated: toggle})
     };
     render() {
         let checkType = this.props.message.isMessageRead ? doubleCheck : oneCheck;
         return (
             <div className={s.message + " " + ((this.props.message.authorId == 'fakeID') ? s.my : s.his)}>
-                <div className={s.messageBox} onDoubleClick={this.activate} onBlur={this.deactivate}>
+                <div className={s.messageBox} onDoubleClick={this.toggle}>
                 <span className={s.body}>{this.props.message.body}</span>
                 <span className={s.date}>{this.props.message.date}</span>
                 {(this.props.message.authorId == 123) &&
