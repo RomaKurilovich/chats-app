35e0375c5815dde35125b8df2124f0c034db019d
LilBudj <kinich2001@gmail.com>
2019-11-23 12:53:00 +0300

    Base UI

---
+++ src/components/Dialogs/Interlocutor.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Interlocutor extends React.Component{
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Interlocutor
