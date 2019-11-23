4f38911141788093df5867dd26d7cf4d124fdbb9
alexviarhey <lexa.vergey@gmail.com>
2019-11-23 14:11:42 +0300

    chat

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import {api} from "./api";
 import Header from "./components/header/Header";
+import openSocket from 'socket.io-client'
 
 class App extends React.Component{
 
@@ -9,7 +10,12 @@ class App extends React.Component{
 
     componentDidMount() {
         api.getKey()
-            .then(token => localStorage.setItem('token', token))
+            .then(token => localStorage.setItem('token', token));
+
+        const socket = openSocket('http://messenger-hackathon.herocuapp.com')
+            socket.on('get-chats-success', (chat => {
+                debugger
+            }))
     }
 
     render(){
