178d846c377422f494105a94582dd9030a87e494
LilBudj <kinich2001@gmail.com>
2019-11-23 17:26:31 +0300

    fix

--- src/components/Dialogs/Dialog.jsx
+++ src/components/Dialogs/Dialog.jsx
@@ -3,18 +3,19 @@ import s from './../Chat.module.css'
 
 class Dialog extends Component {
     render() {
+        debugger
         return (
             <div onClick={() => this.props.setDialogId(this.props.dialog.userId)} className={s.dialog}>
                 <img className={s.photo}
                      src={'https://html.com/wp-content/plugins/htmlcodetutorial-plugin/assets/images/firefox-true.png'}/>
                 <div className={s.dialogInfo}>
                     <div>
-                        <div>{this.props.dialog.interLocuterName}</div>
-                        <div>{this.props.dialog.lastMessageBody} </div>
+                        <div>{this.props.dialog.interlocutor.name}</div>
+                        <div>{this.props.dialog.lastMessage.body} </div>
                     </div>
                     <div>
-                        <div>{this.props.dialog.lastMessageData} </div>
-                        <div>{!this.props.dialog.lastMessageIsRead && this.props.dialog.newMessageCount} </div>
+                        <div>{this.props.dialog.lastMessage.date} </div>
+                        <div>{!this.props.dialog.lastMessage.isRead && this.props.dialog.newMessagesCount} </div>
                     </div>
                 </div>
             </div>
